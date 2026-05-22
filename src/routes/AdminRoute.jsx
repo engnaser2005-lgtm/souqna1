d47d370765b15a99538552bfde1afee5e512776a
engnaser2005-lgtm <engnaser2005@gmail.com>
2026-05-22 22:04:30 +0300

    Create AdminRoute.jsx

---
+++ src/routes/AdminRoute.jsx
@@ -0,0 +1,11 @@
+import ProtectedRoute from './ProtectedRoute'
+
+const AdminRoute = ({ children }) => {
+  return (
+    <ProtectedRoute allowedRoles={['admin']}>
+      {children}
+    </ProtectedRoute>
+  )
+}
+
+export default AdminRoute
